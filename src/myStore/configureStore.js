900b1cdc6e7e29dca0efcfcaf726397fed3e5573
felipe.xavier <felipe.uece@gmail.com>
2020-12-20 19:03:04 -0300

    Add toolkit for redux

--- src/myStore/configureStore.js
+++ src/myStore/configureStore.js
@@ -1,7 +1,7 @@
 import myReducer from "./bugs";
 import createStore from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-export default function configureStore() {
-  const store = createStore(myReducer);
-  return store;
+export default function() {
+  return configureStore({ myReducer });
 };
