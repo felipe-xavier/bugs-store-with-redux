d553ef71828b8d36d56276cbd8a9dd4a77b2bf62
felipe.xavier <felipe.uece@gmail.com>
2020-12-22 23:22:39 -0300

    dispatch actions from diferent reducers.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 import configureStore from "./myStore/configureStore";
-// import { bugAdded, bugRemoved, bugResolved } from "./myStore/bugs";
+import { bugAdded, bugRemoved, bugResolved } from "./myStore/bugs";
+import { getUnresolvedBugs } from './myStore/bugs';
 import { projectAdded } from "./myStore/projects";
 
 const store = configureStore()
 
 store.dispatch(projectAdded({name: 'First Project'}))
-console.log(store.getState())
+// console.log(store.getState())
 
-// const unsubscribe1 = store.subscribe(() => {
-//   console.log("Store changed 1!", store.getState())
-// })
+const unsubscribe1 = store.subscribe(() => {
+  console.log("Store changed 1!", store.getState())
+})
 
-// const unsubscribe2 = store.subscribe(() => {
-//   console.log("Store changed 2!", store.getState())
-// })
+const unsubscribe2 = store.subscribe(() => {
+  console.log("Store changed 2!", store.getState())
+})
 
-// store.dispatch(bugAdded({description: "Bug-1"}));
-// store.dispatch(bugAdded({description: "Bug-2"}));
+store.dispatch(bugAdded({description: "Bug-1"}));
+store.dispatch(bugAdded({description: "Bug-2"}));
 
-// unsubscribe2();
+unsubscribe2();
 
-// store.dispatch(bugResolved({id: 1}));
+store.dispatch(bugResolved({id: 1}));
 
 // store.dispatch(bugRemoved({id: 2}));
+
+console.log(store.getState())
+console.log(getUnresolvedBugs(store.getState()))
