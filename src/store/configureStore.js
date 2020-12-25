5d96f0a43129bf99afc4eb1850fa33def0c3d742
felipe.xavier <felipe.uece@gmail.com>
2020-12-25 19:07:05 -0300

    Setup middleware for the store.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,15 @@
 import reducer from "./reducer";
-import { configureStore } from "@reduxjs/toolkit";
+import logger from "./middlewares/logger";
+import toast from './middlewares/toast';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 export default function() {
-  return configureStore({ reducer });
+  return configureStore({ 
+    reducer,
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({destination: "console"}),
+      toast
+    ]
+  });
 };
