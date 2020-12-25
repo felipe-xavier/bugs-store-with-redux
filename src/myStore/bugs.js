2e47ed2a39c010b2ac8c1f9289a25a1de5903362
felipe.xavier <felipe.uece@gmail.com>
2020-12-25 17:31:04 -0300

    Create the selector for getting bugs assigned to a user.

--- src/myStore/bugs.js
+++ src/myStore/bugs.js
@@ -7,17 +7,18 @@ import { createSelector } from 'reselect';
 // Reducer
 let lastId = 0;
 
-const newBug = description => ({
+const newBug = (description, assignee) => ({
   id: ++lastId,
-  description : description,
+  description,
   resolved : false,
+  assignee,
 })
 
 const slice = createSlice({
   name: 'bugs',
   initialState: [],
   reducers: {
-    bugAdded: (bugs, action) => { bugs.push(newBug(action.payload.description)) },
+    bugAdded: (bugs, action) => { bugs.push(newBug(action.payload.description, action.payload.assignee)) },
 
     bugRemoved: (bugs, action) => {
       const bugId = bugs.findIndex(bug => bug.id == action.payload.id);
@@ -31,11 +32,18 @@ const slice = createSlice({
   }
 })
 
-export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
-export default slice.reducer;
-
-export const getUnresolvedBugs = createSelector(
+const getUnresolvedBugs = createSelector(
   state => state.entities.bugs,
   state => state.entities.projects,
-  (bugs, projects) => bugs.filter(bug => !bug.resolved)
+  bugs => bugs.filter(bug => !bug.resolved)
 )
+
+const getAssigneeBugs = userId => createSelector(
+  state => state.entities.bugs,
+  bugs => bugs.filter(bug => bug.assignee === userId)
+)
+
+export { getAssigneeBugs };
+export { getUnresolvedBugs };
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export default slice.reducer;
