96605b099b01ccdab04235df48c17004ad9d8251
felipe.xavier <felipe.uece@gmail.com>
2020-12-25 17:33:08 -0300

    Add users, set users to bugs and call selectors.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import configureStore from "./myStore/configureStore";
 import { bugAdded, bugRemoved, bugResolved } from "./myStore/bugs";
 import { getUnresolvedBugs } from './myStore/bugs';
+import { getAssigneeBugs } from './myStore/bugs';
 import { projectAdded } from "./myStore/projects";
+import { userAdded } from './myStore/users';
 
 const store = configureStore()
 
-store.dispatch(projectAdded({name: 'First Project'}))
-// console.log(store.getState())
+store.dispatch(projectAdded({ name: 'First Project' }))
 
-const unsubscribe1 = store.subscribe(() => {
-  console.log("Store changed 1!", store.getState())
-})
+store.dispatch(userAdded({ name: 'Felipe' }))
+store.dispatch(userAdded({ name: 'Naiara' }))
 
-const unsubscribe2 = store.subscribe(() => {
-  console.log("Store changed 2!", store.getState())
-})
+store.dispatch(bugAdded({ description: "Bug-1", assignee: 0 }));
+store.dispatch(bugAdded({ description: "Bug-2", assignee: 1 }));
+store.dispatch(bugAdded({ description: "Bug-3" }));
 
-store.dispatch(bugAdded({description: "Bug-1"}));
-store.dispatch(bugAdded({description: "Bug-2"}));
-
-unsubscribe2();
-
-store.dispatch(bugResolved({id: 1}));
-
-store.dispatch(bugRemoved({id: 2}));
+store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugRemoved({ id: 3 }));
 
 console.log(store.getState())
+
 console.log(getUnresolvedBugs(store.getState()))
+
+const bugs = getAssigneeBugs(1)(store.getState())
+console.log(bugs);
