2a8ea8c0516327029d7c217f3662696601b50788
felipe.xavier <felipe.uece@gmail.com>
2020-12-26 16:51:32 -0300

    Create the api middleware.

---
+++ src/store/middlewares/api.js
@@ -0,0 +1,31 @@
+import * as actions from "../api";
+import axios from "axios";
+
+const api = ({ dispatch }) => next => async action => {
+	if (action.type !== actions.apiCallBegun.type) { 
+			return next(action);
+	}
+
+	console.log(action.payload);
+
+	next(action);
+
+	const { url, onSuccess, onFail } = action.payload;
+
+	try {
+		const response = await axios.request({
+			baseURL: "http://localhost:9001/api",
+			url,
+			onSuccess,
+		});
+
+		dispatch(actions.apiCallSucceeded(response.data));
+		if (onSuccess) dispatch({type: onSuccess, payload: response.data});
+	} catch (err) {
+		dispatch(actions.apiCallFailed(err));
+		if (onFail) dispatch({type: onFail, payload: err});
+	}
+	
+}
+
+export default api;
